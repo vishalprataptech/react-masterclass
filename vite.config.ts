b125552acb30c6be4feb93f4d801523efdaf460b
Vishal Pratap Singh <vpsingh3445@gmail.com>
2026-08-12 11:46:56 +0530

    chore: add title

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import {defineConfig} from "vite";
-import {devtools} from "@tanstack/devtools-vite";
-
-import {tanstackStart} from "@tanstack/react-start/plugin/vite";
-
-import viteReact, {reactCompilerPreset} from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import {devtools} from "@tanstack/devtools-vite";
+import {tanstackStart} from "@tanstack/react-start/plugin/vite";
+import viteReact, {reactCompilerPreset} from "@vitejs/plugin-react";
 import {nitro} from "nitro/vite";
+import {defineConfig} from "vite";
 
 const config = defineConfig({
   resolve: {tsconfigPaths: true},
